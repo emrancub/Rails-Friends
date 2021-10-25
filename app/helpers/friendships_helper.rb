8ab869d279ab1d0b816e7265075eaa1422185f2f
emrancub <myself.emranhasan@gmail.com>
2021-10-25 22:50:14 +0600

    Search Added

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,7 +1,6 @@
 module FriendshipsHelper
   def is_requested?(requester_id, addressee_id)
-    Friendship.where(requester_id: requester_id, addressee_id: addressee_id).first &&
-    Friendship.where(requester_id: addressee_id, addressee_id: requester_id).first
+    Friendship.where(requester_id: requester_id, addressee_id: addressee_id).first
   end
 
   def is_connect?(requester_id, addressee_id)
